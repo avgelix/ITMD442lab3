a8c6bbbf3a107549b9e1d66338ece2d49dde5e98
Angela Petrone <apetrone@hawk.iit.edu>
2024-02-27 12:30:16 -0600

    Set the statement to create the Contacts table correctly and implement findAll and create methods

--- src/contactsSQLiteRepository.js
+++ src/contactsSQLiteRepository.js
@@ -1,23 +1,26 @@
 const path = require('node:path');
 const betterSqlite3 = require('better-sqlite3');
+const Contact = require('./Contact');
 
 const db = new betterSqlite3(path.join(__dirname,'../data/contacts.sqlite'), { verbose: console.log }); //verbose is an optional parameter. everytime SQLite does something log it to the console
-const createStmt = db.prepare("CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT)");
+const createStmt = db.prepare("CREATE TABLE IF NOT EXISTS contacts (id INTEGER PRIMARY KEY AUTOINCREMENT, firstName TEXT, lastName TEXT, email VARCHAR(100), notes TEXT, date DATE DEFAULT CURRENT_DATE)");
 createStmt.run();
 
 const repo = {
-    findAll: () => Array.from(db.values()),
+    findAll: () => {
+        const stmt = db.prepare("SELECT * FROM contacts");
+        const rows = stmt.all();
+        let contacts = [];
+        rows.forEach((row) => {
+            const aContact = new Contact(row.id, row.firstName, row.lastName, row.email, row.notes, row.date);
+            contacts.push(aContact);
+        })
+        return contacts;
+    },
     create: (contact) => {
-        // const newContact = {
-        //     id: crypto.randomUUID(),
-        //     firstName: contact.firstName,
-        //     lastName: contact.lastName,
-        //     email: contact.email,
-        //     notes: contact.notes,
-        //     date: new Date().toLocaleDateString('en-us')
-        // };
-        // db.set(newContact.id, newContact);
-        // saveData();
+        const stmt = db.prepare("INSERT INTO contacts (firstName, lastName, email, notes) VALUES (?, ?, ?, ?)");
+        const info = stmt.run(contact.firstName, contact.lastName, contact.email, contact.notes);
+        console.log(`contact created with id: ${info.lastInsertRowid}`);
     },
     findById: (uuid) => db.get(uuid),
     deleteById: (uuid) => {
